6e975f99b82d3cbecc7171c97bff77bc83096d6e
Arnab Koley <arnab123koley@gmail.com>
2024-07-19 08:56:36 +0530

    Project Files Pushed

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCYPHtce8tecUH_JjPaNSxlDQOPGca_Z48",
+  authDomain: "dhunmusicapp.firebaseapp.com",
+  projectId: "dhunmusicapp",
+  storageBucket: "dhunmusicapp.appspot.com",
+  messagingSenderId: "155947212178",
+  appId: "1:155947212178:web:01485caa396302bfb5354b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='w-full h-14 bg-slate-600 flex justify-center items-center text-white font-semibold text-2xl max-md:text-blue-600 max-md:bg-yellow-400'>Dhun app</div>
+      <div className='w-full h-[300px] flex justify-center items-center text-5xl text-green-600 font-bold'>
+        hello
+      </div>
+    </>
+  )
+}
+
+export default App
