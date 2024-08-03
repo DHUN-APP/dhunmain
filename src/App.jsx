e80c65090ea6601637fa25ba93b0b892ab6d0955
Arnab Koley <arnab123koley@gmail.com>
2024-08-04 00:12:50 +0530

    logo animation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AuthLogin from "./Auth/AuthLogin";
 import CreateProfile from './Components/CreateProfile/CreateProfile';
@@ -13,17 +13,39 @@ import ProtectedRoute from './Context/ProtectedRoute';
 function App() {
 
   const [userType,setUserType] = useState();
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const splashTimeout = setTimeout(() => {
+      setShowSplash(false);
+    }, 5100);
+
+    return () => clearTimeout(splashTimeout);
+  }, []);
 
   return (
     <>
       <AuthProvider>
         <Router>
+        {showSplash ? (
+            <div className="flex flex-col items-center justify-center h-screen bg-black">
+              <video
+                  src="/assets/sqrlogo.mp4"
+                  autoPlay
+                  loop
+                  muted
+                  className="w-auto h-screen max-md:w-screen max-md:h-auto"
+                >
+              </video>
+            </div>
+          ) : (
             <Routes>
               <Route path="/" element={<AuthLogin setUserType={setUserType} />} />
               <Route path="/createprofile/:section" element={<ProtectedRoute><CreateProfile /></ProtectedRoute>} />
               <Route path="/app/:section" element={<ProtectedRoute><Layout userType={userType} setUserType={setUserType} /></ProtectedRoute>} />
               <Route path="*" element={<ProtectedRoute><NavigationError /></ProtectedRoute>} />
             </Routes>
+          )}
         </Router>
         <ToastContainer
           position="top-center"
