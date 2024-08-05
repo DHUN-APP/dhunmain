c347e514ff5695fc62fe485fff325014e215f3df
Tanmoy-Santra <tanmoysantra911@gmail.com>
2024-08-05 13:00:48 +0530

    update plalylist feature

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,54 +1,58 @@
-import React, {useState} from 'react'
-import Home from '../Home/Home'
-import '../../App.css'
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Settings from '../Settings/Settings';
-import MyProfile from '../MyProfile/MyProfile';
+import Home from '../Home/Home';
 import Search from '../Search/Search';
-import Notifications from '../Notifications/Notifications';
-import ProPlan from '../ProPlan/ProPlan';
-import AddSong from '../MySong/AddSong';
+import MyProfile from '../MyProfile/MyProfile';
 import SongDetails from '../MySong/SongDetails';
-import { useParams } from 'react-router-dom';
-import EditInfo from '../MyProfile/EditInfo';
+import AddSong from '../MySong/AddSong';
 import EditSong from '../MySong/EditSong';
+import Notifications from '../Notifications/Notifications';
+import Settings from '../Settings/Settings';
+import ProPlan from '../ProPlan/ProPlan';
+import EditInfo from '../MyProfile/EditInfo';
+import PlaylistDetails from '../MyPlaylist/PlaylistDetails';
+import AddPlaylist from '../MyPlaylist/AddPlaylist';
+import EditPlaylist from '../MyPlaylist/EditPlaylist';
 
-const Main = ({section, userType, setUserType, songId, setSongId}) => {
-
+const Main = ({ section, userType, setUserType, songId, setSongId, playlistId, setPlaylistId }) => {
   const navigate = useNavigate();
 
-    const renderComponent = () => {
-        switch (section) {
-          case "home":
-            return <Home userType={userType} setUserType={setUserType} />;
-          case "search":
-            return <Search/>
-          case "myprofile":
-            return <MyProfile setSongId={setSongId}/>
-          case "song":
-            return <SongDetails songId={songId}/>
-          case "addsong":
-            return <AddSong/>
-          case "editsong":
-            return <EditSong songId={songId}/>
-          case "notifications":
-            return <Notifications/>
-          case "settings":
-            return <Settings/>
-          case "proplan":
-            return <ProPlan/>
-          case "editinfo":
-            return <EditInfo/>
-          default:
-            return <Home userType={userType} setUserType={setUserType} />;
-        }
-      };
+  const renderComponent = () => {
+    switch (section) {
+      case "home":
+        return <Home userType={userType} setUserType={setUserType} />;
+      case "search":
+        return <Search />;
+      case "myprofile":
+        return <MyProfile setSongId={setSongId} setPlaylistId={setPlaylistId} />;
+      case "song":
+        return <SongDetails songId={songId} />;
+      case "addsong":
+        return <AddSong />;
+      case "editsong":
+        return <EditSong songId={songId} />;
+      case "notifications":
+        return <Notifications />;
+      case "settings":
+        return <Settings />;
+      case "proplan":
+        return <ProPlan />;
+      case "editinfo":
+        return <EditInfo />;
 
-  return (
-    <div>
-        {renderComponent()}
-    </div>
-  )
-}
+     
+      case "playlist":
+        return <PlaylistDetails playlistId={playlistId} />;
+      case "addplaylist":
+        return <AddPlaylist />;
+      case "editplaylist":
+        return <EditPlaylist playlistId={playlistId} />;
+      default:
+        return <Home userType={userType} setUserType={setUserType} />;
+    }
+  };
 
-export default Main
+  return <div>{renderComponent()}</div>;
+};
+
+export default Main;
